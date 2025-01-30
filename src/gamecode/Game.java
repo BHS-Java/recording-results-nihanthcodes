8c6330e23315faa16a3050bc9ac883bb77932cfc
Nihanth Tatikonda <2015567@apps.nsd.org>
2025-01-30 14:01:47 -0800

    real hw1

--- src/gamecode/Game.java
+++ src/gamecode/Game.java
@@ -1,5 +1,9 @@
 package gamecode;
 
 public class Game {
+    Player player1;
 
+    public Game(Player player){
+        this.player1 = player;
+    }
 }
--- src/gamecode/Main.java
+++ src/gamecode/Main.java
@@ -1,16 +1,30 @@
 package gamecode;
+import java.util.Scanner;
 
 public class Main implements Spec{
     public static void main(String[] args) {
-        
+        Main mainInstance = new Main(); 
+        String name = mainInstance.askString("whats ur name: ");
+
+        int height = mainInstance.askNumber("what height r u: ");
+        int gradeLevel = mainInstance.askNumber("what grade r u in: ");
+
+        Player player1 = new Player(name, height, gradeLevel);
+
     }
 
     public int askNumber(String question){
-        return 0;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println(question);
+        int num = Integer.parseInt(myObj.nextLine());
+        return num;
     }
 
     public String askString(String question){
-        return "";
+        Scanner myObj = new Scanner(System.in);
+        System.out.println(question);
+        String response = myObj.nextLine();
+        return response;
     }
 
     public Results getResults(Game guessingOrBetter){
